512a7bb477137aa67717ceee7bd3d062ffb7cf98
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2021-12-08 14:27:10 +0100

    Enemy movement (#2)
    * add git files
    
    * add unity project
    
    * add sprites and tiles
    
    * Update SampleScene.unity
    
    * add enemy moving
    
    -move between A and B
    -move to player when in area
    
    * add stoping enemy when near player

---
+++ Assets/Scripts/ABMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABMovement : MonoBehaviour
+{
+    [SerializeField] Vector3 pointA;
+    [SerializeField] Vector3 pointB;
+    public Vector3 actualPoint;
+    [SerializeField] float movementSpeed;
+    public bool ifAggro = false;
+
+    [SerializeField] EnterArea protectedArea;
+
+    private void Start()
+    {
+        actualPoint = pointA;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ifAggro = protectedArea.ifPlayerIn;
+
+        if(!ifAggro)
+        {
+            if (transform.position.x == pointA.x) actualPoint = pointB;
+            else if (transform.position.x == pointB.x) actualPoint = pointA;
+
+            transform.position = Movement(actualPoint, movementSpeed);
+        }
+        if(ifAggro)
+        {
+            ColliderDistance2D colliderDistance = 
+                gameObject.GetComponent<Collider2D>()
+                .Distance(protectedArea.playerPosition.GetComponent<Collider2D>());
+
+            if(colliderDistance.distance > 0.05f)
+            {
+                transform.position = 
+                    Movement(protectedArea.playerPosition.transform.position, movementSpeed * 2);
+            }
+            
+        }
+    }
+    Vector3 Movement(Vector3 position, float speed)
+    {
+        return Vector2.MoveTowards(transform.position, position, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/EnterArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterArea : MonoBehaviour
+{
+    internal bool ifPlayerIn = false;
+    internal GameObject playerPosition;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            ifPlayerIn = true;
+            playerPosition = collision.gameObject;
+        }
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            ifPlayerIn = false;
+        }
+    }
+}
