74ec23e46bf74200def89e5c3d6b3d74f9a3da3e
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-12-08 14:06:34 +0100

    add enemy moving
    -move between A and B
    -move to player when in area

---
+++ Assets/Scripts/ABMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABMovement : MonoBehaviour
+{
+    [SerializeField] Vector3 pointA;
+    [SerializeField] Vector3 pointB;
+    public Vector3 actualPoint;
+    [SerializeField] float movementSpeed;
+    public bool ifAggro = false;
+
+    [SerializeField] EnterArea protectedArea;
+
+    private void Start()
+    {
+        actualPoint = pointA;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ifAggro = protectedArea.ifPlayerIn;
+
+        if(!ifAggro)
+        {
+            if (transform.position.x == pointA.x) actualPoint = pointB;
+            else if (transform.position.x == pointB.x) actualPoint = pointA;
+
+            transform.position = Movement(actualPoint, movementSpeed);
+        }
+        if(ifAggro)
+        {
+            transform.position = Movement(protectedArea.playerPosition, movementSpeed * 2);
+        }
+    }
+    Vector3 Movement(Vector3 position, float speed)
+    {
+        return Vector2.MoveTowards(transform.position, position, movementSpeed * Time.deltaTime);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EnterArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterArea : MonoBehaviour
+{
+    internal bool ifPlayerIn = false;
+    internal Vector2 playerPosition;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            ifPlayerIn = true;
+            playerPosition = collision.gameObject.transform.position;
+        }
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            ifPlayerIn = false;
+        }
+    }
+}
