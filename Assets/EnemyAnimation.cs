03bc3adad189fb61709fe0ca20ea1f4d709c979c
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2021-12-09 14:22:04 +0100

    Task2 (#3)
    * move minotaur files
    
    * add wrath asset
    
    * install animation
    
    * fixed files
    
    * import wrath components
    
    * add a* package
    
    * add animated wrath following player
    
    * Update Player.prefab & scene
    
    * add video

---
+++ Assets/EnemyAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    public AIPath aiPath;
+    float xScale;
+    private void Start()
+    {
+        xScale = transform.localScale.x;
+    }
+    void Update()
+    {
+        if(aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(xScale, transform.localScale.y, transform.localScale.z);
+            
+        }
+        else if(aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-xScale, transform.localScale.y, transform.localScale.z);
+        }
+    }
+}
